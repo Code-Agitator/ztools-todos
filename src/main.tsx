627baaa6a58c37d0b013ca4ca3ff77cb5d255bc5
Agitator <1482084534@qq.com>
2026-07-08 09:59:16 +0800

    feat(styles): add CSS variables and global styles (task 5.1)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './main.css'
 import App from './App'
+import './styles'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
